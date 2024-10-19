42979a09df49b9837a275182f0f395117b978f51
Aina Orozco Gonzalez <468417.joan23@fje.edu>
2024-10-19 13:00:45 +0200

    Añadida la clase Circulo

---
+++ Clases/Circulo.php
@@ -0,0 +1,30 @@
+<?php
+    // CLASE CIRCULO QUE HEREDA DE FIGURAGEOMETRICA
+    class Circulo extends Figurageometrica {
+
+        // CONSTRUCTOR
+        public function __construct($lado1) {
+            parent::__construct("Circulo", $lado1);
+        }
+
+        // METODO PARA CALCULAR EL AREA DEL CIRCULO
+        public function calcularArea() {
+            return pi() * pow($this->lado1, 2);
+        }
+
+        // METODO PARA CALCULAR EL PERIMETRO DEL CIRCULO
+        public function calcularPerimetro() {
+            return 2 * pi() * $this->lado1;
+        }
+
+        // METODO TOSTRING PARA LA DESCRIPCION
+        public function __toString() {
+            return "Tipo de figura; $this->tipoFigura, Radio: $this->lado1";
+        }
+
+        // DESTRUCTOR
+        public function __destruct() {
+        }
+    }
+        
+?>
