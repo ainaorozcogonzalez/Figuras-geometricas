39ae81bf261b13790b360a55c860e1afd9f8abaf
Aina Orozco Gonzalez <468417.joan23@fje.edu>
2024-10-19 12:22:51 +0200

    Añadida la clase base FiguraGeometrica.php

---
+++ Clases/FiguraGeometrica.php
@@ -0,0 +1,36 @@
+<?php
+
+    // CLASE PARA TODAS LAS FIGURAS GEOMÉTICAS
+    class FiguraGeometrica {
+        public $tipofigura;
+        public $lado1;
+
+        // CONSTRUCTOR PARA INICIALIZAR LOS ATRIBUTOS
+        public function __construct($tipofigura, $lado1) {
+            $this->tipofigura = $tipofigura;
+            $this->lado1 = $lado1;
+        }
+
+        // GETERS Y SETTERS
+        public function getTipofigura() {
+            return $this->tipofigura;
+        }
+
+        public function setTipofigura($tipofigura) {
+            $this->tipofigura = $tipofigura;
+        }
+
+        public function getLado1() {
+            return $this->lado1;
+        }
+
+        public function setLado1($lado1) {
+            $this->lado1 = $lado1;
+        }
+
+        // DESTRUCTOR
+        public function __destruct() {
+
+        }
+    }
+?>
